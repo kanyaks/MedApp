48878b19e3f373346d96c1142375373e1d7d2d75
Kanya Rajan <Raj@Kanyas-MacBook-Pro.local>
2017-02-12 19:40:55 +0530

    Initial Commit

---
+++ MedAppointment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MedAppointment
+//
+//  Created by Kanya Rajan on 2/12/17.
+//  Copyright © 2017 Kanya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
